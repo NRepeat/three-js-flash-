60d7da148686d19728165bfc168f364ca51dc8e5
Nikita Nazarov <nnazarov55@gmail.com>
2023-02-27 01:16:34 +0200

    made a change snap test

--- txtsnap.cpp
+++ txtsnap.cpp
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> testing
 #include <iostream>
 using namespace std;
  
  int main() {
     cout<<"Hello world";
-<<<<<<< HEAD
     return 0;
-=======
-    return 1;
->>>>>>> testing
  }
